76174cf301dad533fa63c3296edd4d10338f3a56
Bishoy Romany <7bishoyromany7@gmail.com>
2020-06-04 23:20:25 +0200

    Clean UP

--- backend/serve.php
+++ backend/serve.php
@@ -7,18 +7,6 @@ require_once "helpers.php";
 if(isset($_GET['serve'])):
     $tokens = json_decode(file_get_contents(__DIR__."/../tokens.json"), true);
 
-    // out     = json
-    // qt      = tiktok
-    // results = 1
-    // v       = 7
-    // ip      = 96.41.207.187
-    // ua      = Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36
-    // partner = trendqubeals
-    // sub1    = tiktok
-    // sub2    = com.chess
-    // m-aaid  = a39404a7-59cb-4352-8501-458d7da59ae2
-    // rfr     = https://play.google.com/store/apps/details?id=com.chess
-
     $validator = validate('qt', 'sub1', 'sub2', 'm-aaid');
 
     if($validator !== true){
