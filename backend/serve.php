d74c6b2717f3534acfc87fca9c6bc5796ac86845
Bishoy Romany <7bishoyromany7@gmail.com>
2020-06-17 12:21:17 +0200

    Cleanup serve file

--- backend/serve.php
+++ backend/serve.php
@@ -1,14 +1,24 @@
 <?php 
 
+/**
+ * include the helpers functions [dd, validate, addHttp, post]
+ */
 require_once "helpers.php";
 
 
 
 if(isset($_GET['serve'])):
+    /**
+     * get the API tokens
+     */
     $tokens = json_decode(file_get_contents(__DIR__."/../tokens.json"), true);
-
+    /**
+     * validate the get request
+     */
     $validator = validate('qt', 'sub1', 'sub2', 'm-aaid');
-
+    /**
+     * check if validation success
+     */
     if($validator !== true){
         echo $validator;
         exit;
@@ -17,178 +27,92 @@ if(isset($_GET['serve'])):
     /**
      * Admarketplace API
      */
-
+    // search keyword
     $qt = $_GET['qt'];
+    // sub1 single data
     $sub1 = $_GET['sub1'];
+    // sub2 single check
     $sub2 = $_GET['sub2'];
     $m_aaid = $_GET['m-aaid'];
+    // sub2 list
+    $sub2List = $tokens['sub2'];
+    // the added offers list
+    $addedOffers = [];
+    /**
+     * check if request for a list or a single sub2
+     */
+    if(empty($sub2List)){
+        $sub2List = [$sub2];
+    }
 
-    $admarketplace = $tokens['admarketplace'];
-
-    $admarketplaceQuery = $admarketplace['requestAddon'];
-    $admarketplaceQuery['qt'] = $qt;
-    $admarketplaceQuery['sub1'] = $sub1;
-    $admarketplaceQuery['sub2'] = $sub2;
-    $admarketplaceQuery['m-aaid'] = $m_aaid;
-
-    $admarketplaceUrl = $admarketplace['url'].'?'.http_build_query($admarketplaceQuery);
-
-    $admarketplaceResponse = json_decode(file_get_contents($admarketplaceUrl));
-
-    $ads = $admarketplaceResponse->adlistings->listing;
-
-    // dd($ads);
-    foreach($ads as $ad):
+    /**
+     * loop all sub2 values to store the data for affise API
+     */
+    foreach($sub2List as $sub2):
         /**
-         * Affise API
+         * get admarketplace offets
          */
-        $affise = $tokens['affise'];
-        $affiseUrl = $affise['url'];
-        $affiseHeaders = $affise['headers'];
-        $affiseParams = $affise['requestAddon'];
+        $admarketplace = $tokens['admarketplace'];
 
-        // title=test&advertiser=573c69a33b7d9b0e638b4576&url=http://example.com&url_preview=http://preview.example.com
-        $affiseParams['title'] = $ad->title;
-        $affiseParams['url'] = $ad->clickurl;
-        // $affiseParams['domain_url'] = $ad->clickurl;
-        $affiseParams['url_preview'] = addhttp($ad->displayurl);
-        $affiseParams['status'] = 'active';
-        $affiseParams['sub_account_1'] = $sub1;
-        $affiseParams['sub_account_2'] = $sub2;
-        $affiseParams['description_lang'] = [
-            'en' => $ad->description,
-        ];
-        $affiseParams['external_offer_id'] = $ad->adId;
+        $admarketplaceQuery = $admarketplace['requestAddon'];
+        $admarketplaceQuery['qt'] = $qt;
+        $admarketplaceQuery['sub1'] = $sub1;
+        $admarketplaceQuery['sub2'] = $sub2;
+        $admarketplaceQuery['m-aaid'] = $m_aaid;
 
-        // payment
-        $affiseParams['payments'] = [];
-        $affiseParams['payments']['country_exclude'] = false;
-        $affiseParams['payments']['countries'] = '';
-        $affiseParams['payments']['cities'] = '';
-        $affiseParams['payments']['goal'] = '';
-        $affiseParams['payments']['total'] = '';
-        $affiseParams['payments']['revenue'] = '';
-        $affiseParams['payments']['currency'] = 'usd';
-        $affiseParams['payments']['type'] = 'fixed';
-        $affiseParams['payments']['sub1'] = $sub1;
-        $affiseParams['payments']['sub2'] = $sub2;
+        $admarketplaceUrl = $admarketplace['url'].'?'.http_build_query($admarketplaceQuery);
 
-        // "payments":[
-        //     {
-        //        "countries":[
-   
-        //        ],
-        //        "cities":[
-   
-        //        ],
-        //        "devices":[
-   
-        //        ],
-        //        "os":[
-   
-        //        ],
-        //        "goal":"1",
-        //        "total":0,
-        //        "revenue":0,
-        //        "currency":"usd",
-        //        "title":"",
-        //        "type":"fixed",
-        //        "url":null,
-        //        "country_exclude":false,
-        //        "with_regions":false,
-        //        "sub1":null,
-        //        "sub2":null,
-        //        "sub3":null,
-        //        "sub4":null,
-        //        "sub5":null,
-        //        "sub6":null,
-        //        "sub7":null,
-        //        "sub8":null
-        //     }
-        //  ],
+        $admarketplaceResponse = json_decode(file_get_contents($admarketplaceUrl));
 
-        dd($affiseParams);
+        $ads = $admarketplaceResponse->adlistings->listing;
 
-        // dd($affiseParams);
+        /**
+         * loop admarketplace offers
+         */
+        foreach($ads as $ad):
+            /**
+             * Affise API
+             */
+            $affise = $tokens['affise'];
+            $affiseUrl = $affise['url'];
+            $affiseHeaders = $affise['headers'];
+            $affiseParams = $affise['requestAddon'];
+            $affiseParams['title'] = $ad->title . ' - '. $sub2;
+            $affiseParams['url'] = $ad->clickurl;
+            $affiseParams['url_preview'] = addhttp($ad->displayurl);
+            $affiseParams['status'] = 'active';
+            $affiseParams['sub_account_1'] = $sub1;
+            $affiseParams['sub_account_2'] = $sub2;
+            $affiseParams['description_lang'] = [
+                'en' => $ad->description,
+            ];
+            $affiseParams['external_offer_id'] = $ad->adId;
 
-        // $affiseParams['hide_payments'] = false;
-        // $affiseParams['title'] = 'This is a test offer';
-        // $affiseParams['macro_url'] = null;
-        // $affiseParams['url'] = 'http://affise.com';
-        // $affiseParams['cross_postback_url'] = 'http://test-url.com';
-        // $affiseParams['url_preview'] = 'http://preview.affise.com';
-        // $affiseParams['preview_url'] = 'http://preview.affise.com';
-        // $affiseParams['domain_url'] = 'affise.tds';
-        // $affiseParams['use_https'] = false;
-        // $affiseParams['use_http'] = true;
-        // $affiseParams['description_lang'] = [
-        //     'ru' => 'Описание',
-        //     'en' => 'Description'
-        // ];
-        // $affiseParams['sources'] = [];
-        // $affiseParams['logo'] = '/images/cpa/logos/';
-        // $affiseParams['status'] = 'stopped';
-        // $affiseParams['tags'] = ['default'];
-        // $affiseParams['privacy'] = 'public';
-        // $affiseParams['is_top'] = 0;
-        // $affiseParams['payments'] = [];
-        // $affiseParams['partner_payments'] = [];
-        // $affiseParams['landings'] = [];
-        // $affiseParams['strictly_country'] = 0;
-        // $affiseParams['strictly_os'] = [];
-        // $affiseParams['strictly_connection_type'] = 'wi-fi';
-        // $affiseParams['is_redirect_overcap'] = false;
-        // $affiseParams['notice_percent_overcap'] = null;
-        // $affiseParams['hold_period'] = 0;
-        // $affiseParams['categories'] = [];
-        // $affiseParams['full_categories'] = [];
-        // $affiseParams['cr'] = 0;
-        // $affiseParams['epc'] = 0;
-        // $affiseParams['notes'] = null;
-        // $affiseParams['allowed_ip'] = '';
-        // $affiseParams['hash_password'] = null;
-        // $affiseParams['allow_deeplink'] = 0;
-        // $affiseParams['hide_referer'] = 0;
-        // $affiseParams['start_at'] = "2019-06-17 12:35:00";
-        // $affiseParams['stop_at'] = null;
-        // $affiseParams['auto_offer_connect'] = null;
-        // $affiseParams['required_approval'] = false;
-        // $affiseParams['is_cpi'] = false;
-        // $affiseParams['creatives'] = [];
-        // $affiseParams['creatives_zip'] = null;
-        // $affiseParams['smartlink_categories'] = ["595e3b5b7e28fede7b8b456d"];
-        // $affiseParams['click_session'] = "1y";
-        // $affiseParams['minimal_click_session'] = "0s";
-        // $affiseParams['external_offer_id'] = "this_is_my_test_id";
-        // $affiseParams['sub_account_1'] = 'first_sub_value';
-        // $affiseParams['sub_account_2'] = 'second_sub_value';
-        // // $affiseParams['sub_restrictions'] = [
-        // //     'sub1' => 'first_sub_value',
-        // //     'sub2' => 'second_sub_value'
-        // // ];
-        // $affiseParams['caps_timezone'] = "Europe/Moscow";
-        // $affiseParams['strictly_isp'] = ['595e3b5b7e28fede7b8b456d'];
-        // $affiseParams['note_aff'] = "";
-        // $affiseParams['note_sales'] = "";
-        // $affiseParams['disallowed_ip'] = "";
-        // $affiseParams['hide_caps'] = 0;
-        // $affiseParams['caps_status'] = ['confirmed'];
-        // $affiseParams['caps_goal_overcap'] = "install";
-        // $affiseParams['commission_tiers'] = [
-        //     'affiliates' => [1],
-        //     'goal' => [],
-        //     'timeframe' => 'month',
-        //     'type' => 'budget',
-        //     'value' => 0.5,
-        //     'target_goals' => [],
-        //     'modifier_type' => 'to_percent',
-        //     'modifier_value' => 10.02
-        // ];
+            /**
+             * payment part
+             * un comment only if you are going to add the payment stuff here
+             */
+            // $affiseParams['payments'] = [];
+            // $affiseParams['payments']['country_exclude'] = false;
+            // $affiseParams['payments']['countries'] = '';
+            // $affiseParams['payments']['cities'] = '';
+            // $affiseParams['payments']['goal'] = '';
+            // $affiseParams['payments']['total'] = '';
+            // $affiseParams['payments']['revenue'] = '';
+            // $affiseParams['payments']['currency'] = 'usd';
+            // $affiseParams['payments']['type'] = 'fixed';
+            // $affiseParams['payments']['sub1'] = $sub1;
+            // $affiseParams['payments']['sub2'] = $sub2;
 
-        // dd($affiseParams);
-
-        $data = post($affiseUrl, $affiseParams, $affiseHeaders);
-
-        dd($data);
+            // send the request to affise api to store the data
+            $data = post($affiseUrl, $affiseParams, $affiseHeaders);
+            // add offer to the added offers
+            array_push($addedOffers, $data);
+        endforeach;
     endforeach;
+
+    /**
+     * show the result of added offers
+     */
+    dd(["offsers" => $addedOffers, "countOfAddedOffers" => count($addedOffers)]);
 endif;
